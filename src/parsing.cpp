f8da15193b6c207a53a1d2654b0387ac798b4f3a
Pierre Rosset <prosset@1B5.42angouleme.fr>
2025-10-01 16:19:21 +0200

    parsing kick et privmsg

--- src/parsing.cpp
+++ src/parsing.cpp
@@ -6,7 +6,7 @@
 /*   By: prosset <prosset@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 10:11:18 by prosset           #+#    #+#             */
-/*   Updated: 2025/09/24 13:39:25 by prosset          ###   ########.fr       */
+/*   Updated: 2025/10/01 16:18:07 by prosset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,15 @@ void pars_nick(std::string str, Server &serv)
 {
 	std::vector<Client> clients = serv.getClients();
 
+	if (str.empty())
+	{
+		std::cerr << "Error : no nickname given." << std::endl;
+		return ;
+	}
+
 	if (str.size() > 9)
 	{
-		std::cerr << "Please enter a nickname of maximum 9 characters." << std::endl;
+		std::cerr << "Error : please enter a nickname of maximum 9 characters." << std::endl;
 		return ;
 	}
 	
@@ -27,7 +33,7 @@ void pars_nick(std::string str, Server &serv)
 	{
 		if (str == clients[i].getNickname())
 		{
-			std::cerr << "Nickname unavailable." << std::endl;
+			std::cerr << "Error : nickname unavailable." << std::endl;
 			return ;
 		}
 	}
@@ -49,13 +55,13 @@ void pars_user(std::string str, Server &serv)
 	}
 	if (count < 3)
 	{
-		std::cerr << "Need more parameters." << std::endl;
+		std::cerr << "Error : need more parameters." << std::endl;
 		return ;
 	}
 
 	for (int i = 0; i < 3; i++)
 	{
-		while (str[index] != ' ')
+		while (index < str.find(' '))
 		{
 			args[i] += str[index];
 			index++;	
@@ -72,7 +78,7 @@ void pars_user(std::string str, Server &serv)
 	{
 		if (args[0] == clients[i].getUsername())
 		{
-			std::cerr << "Username unavailable." << std::endl;
+			std::cerr << "Error : username unavailable." << std::endl;
 			return ;
 		}
 	}
@@ -81,10 +87,12 @@ void pars_user(std::string str, Server &serv)
 	{
 		if (args[1][i] < '0' || args[1][i] > '9')
 		{
-			std::cerr << "Usermod should be a numeric." << std::endl;
+			std::cerr << "Error : usermod should be a numeric." << std::endl;
 			return ;
 		}
 	}
+
+	// ERR_ALREADYREGISTRED //
 
 	// Lancer la commande Username //
 }
@@ -101,7 +109,7 @@ void pars_join(std::string str, Server &serv)
 	std::vector<std::string> keys;
 	
 	size_t i = 0;
-	while (i < str.size() && str[i] != ' ')
+	while (i < str.size() && i < str.find(' '))
 	{
 		if (str[i] == ',')
 			i++;
@@ -128,6 +136,12 @@ void pars_join(std::string str, Server &serv)
 		keys.push_back(key);
 	}
 
+	if (keys.empty())
+	{
+		std::cerr << "Error : need more params." << std::endl;
+		return ;
+	}
+
 	// i = 0;
 	// std::vector<Channel> channels = serv.getChannels();
 	// while (i <= chans.size())
@@ -141,32 +155,53 @@ void pars_join(std::string str, Server &serv)
 	// 			if (keys[i] != channels[n].getChanKey())
 	// 			{
 	// 				std::cerr << "Error : wrong channel key for channel " << chans[i] << "." << std::endl;
-	// 				return ;
+	// 				chans[i] = "";
 	// 			}
 	// 			if (channels[n].getInvite())
 	// 			{
-	// 				std::cerr << "Error : you tried to join an invite only channel : " << channels[n].getChanName() << "." << std::endl;
-	// 				return ; 
+	// 				std::cerr << "Error : you tried to join an invite only channel : " << chans[i] << "." << std::endl;
+	// 				chans[i] = "";
+	// 			}
+	// 			if (channels[n].getMembers().size() == 20)
+	// 			{
+	// 				std::cerr << "Error : channel " << chans[i] << " is full." << std::endl;
+	// 				chans[i] = "";
 	// 			}
 	// 		}
 	// 	}
 	// 	if (!chan_exist)
 	// 	{
 	// 		std::cerr << "Error : no such channel as " << chans[i] << "." << std::endl;
-	// 		return ;
+	// 		chans[i] = "";
 	// 	}
 	// 	i++;
 	// }
-
 	
+	// ERR_BADCHANMASK //
+
+	// ERR_BANNEDFROMCHAN //
+
+	// ERR_TOOMANYTARGETS //
+
+	// ERR_UNAVAILRESOURCE //
+
+	// ERR_TOOMANYCHANNELS //
+
+	// RPL_TOPIC //
 }
 
-void pars_part(std::string str)
+void pars_part(std::string str, Server &serv)
 {
 	std::vector<std::string> chans;
 	
+	if (str.empty())
+	{
+		std::cerr << "Error : need more params." << std::endl;
+		return ;
+	}
+
 	size_t i = 0;
-	while (i < str.size() && str[i] != ' ')
+	while (i < str.size() && i < str.find(' '))
 	{
 		if (str[i] == ',')
 			i++;
@@ -190,6 +225,24 @@ void pars_part(std::string str)
 		}
 	}
 
+	// for (size_t j = 0; j < chans.size(); j++)
+	// {
+	// 	std::vector<Channel> channels = serv.getChannels();
+	// 	bool chan_exist = 0;
+	// 	for (size_t i = 0; i < channels.size(); i++)
+	// 	{
+	// 		if (chans[j] == channels[i].getChanName())
+	// 			chan_exist = 1;
+	// 	}
+	// 	if (!chan_exist)
+	// 	{
+	// 		std::cerr << "Error : no such channel as " << chans[j] << "." << std::endl;
+	// 		chans[j] = "";
+	// 	}
+	// }
+
+	// ERR_NOTONCHANNEL //
+
 	// Lancer la commande PART //
 }
 
@@ -205,7 +258,7 @@ void pars_topic(std::string str, Server &serv)
 	}
 	
 	size_t i = 0;
-	while (i < str.size() && str[i] != ' ')
+	while (i < str.size() && i < str.find(' '))
 	{
 		chan += str[i];
 		i++;
@@ -245,7 +298,7 @@ void pars_invite(std::string str, Server &serv)
 	std::string chan;
 
 	size_t i = 0;
-	while (i < str.size() && str[i] != ' ')
+	while (i < str.size() && i < str.find(' '))
 	{
 		nick += str[i];
 		i++;
@@ -277,23 +330,154 @@ void pars_invite(std::string str, Server &serv)
 		return ;
 	}
 
-	// ERR_NOTONCHANNEL //
+	// std::vector<Channel> channels = serv.getChannels();
+	// for (size_t i = 0; i < channels.size(); i++)
+	// {
+	// 	if (chan == channels[i].getChanName())
+	// 	{
+	// 		std::vector<Client> chan_members = channels[i].getMembers();
+	// 		for (size_t j = 0; j < chan_members.size(); j++)
+	// 		{
+	// 			if (nick == chan_members[j].getNickname())
+	// 			{
+	// 				std::cerr << "Error : user is already on the channel." << std::endl;
+	// 				return ;
+	// 			}
+	// 		}
+			
+	// 	}
+	// }
 
-	// ERR_USERONCHANNEL //
+	// ERR_NOTONCHANNEL //
 
     // ERR_CHANOPRIVSNEEDED //
 
 	// Lancer la commande INVITE //
 }
 
-void pars_kick(std::string str)
+void pars_kick(std::string str, Server &serv)
 {
-	 
-}
+	std::vector<std::string> chans;
 
-void pars_quit(std::string str)
-{
-	// Lancer la commande QUIT //
+	size_t i = 0;
+	while (i < str.size() && i < str.find(' '))
+	{
+		if (str[i] == ',')
+			i++;
+		std::string chan;
+		while (i < str.size() && str[i] != ' ' && str[i] != ',')
+		{
+			chan += str[i];
+			i++;
+		}
+		chans.push_back(chan);
+	}
+
+	std::vector<std::string> users;
+
+	i++;
+	while (i < str.size() && i < str.find(' '))
+	{
+		if (str[i] == ',')
+			i++;
+		std::string user;
+		while (i < str.size() && str[i] != ' ' && str[i] != ',')
+		{
+			user += str[i];
+			i++;
+		}
+		users.push_back(user);
+	}
+
+	std::string comment;
+	i++;
+	while (i < str.size())
+	{
+		comment += str[i];
+		i++;
+	}
+
+	if (users.empty())
+	{
+		std::cerr << "Error : need more params." << std::endl;
+		return ;
+	}
+	
+	if (chans.size() != 1 && chans.size() != users.size())
+	{
+		std::cerr << "Error : there must be either one channel or as many channels as users." << std::endl;
+		return ;
+	}
+
+	// if (chans.size() == 1)
+	// {
+	// 	std::vector<Channel> channels = serv.getChannels();
+	// 	bool chan_exist = 0;
+	// 	for (size_t i = 0; i < channels.size(); i++)
+	// 	{
+	// 		if (chans[0] == channels[i].getChanName())
+	// 		{
+	// 			chan_exist = 1;
+	// 			std::vector<Client> chan_members = channels[i].getMembers();
+	// 			for (size_t n = 0; n < users.size(); n++)
+	// 			{
+	// 				bool user_exist = 0;
+	// 				for (size_t j = 0; j < chan_members.size(); j++)
+	// 				{
+	// 					if (users[n] == chan_members[j].getNickname())
+	// 						user_exist = 1;
+	// 				}
+	// 				if (!user_exist)
+	// 				{
+	// 					std::cerr << "Error : user " << users[n] << " is not on channel" << chans[0] << "." << std::endl;
+	// 					users[n] = "";
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// }
+	// else
+	// {
+	// 	for (size_t j = 0; j < chans.size(); j++)
+	// 	{
+	// 		std::vector<Channel> channels = serv.getChannels();
+	// 		bool chan_exist = 0;
+	// 		for (size_t i = 0; i < channels.size(); i++)
+	// 		{
+	// 			if (chans[j] == channels[i].getChanName())
+	// 			{
+	// 				chan_exist = 1;
+	// 				std::vector<Client> chan_members = channels[i].getMembers();
+	// 				bool user_exist = 0;
+	// 				for (size_t n = 0; n < chan_members.size(); n++)
+	// 				{
+	// 					if (users[j] == chan_members[n].getNickname())
+	// 						user_exist = 1;
+	// 				}
+	// 				if (!user_exist)
+	// 				{
+	// 					std::cerr << "Error : user " << users[j] << " is not on channel" << chans[j] << "." << std::endl;
+	// 					users[j] = "";
+	// 					chans[j] = "";
+	// 				}
+	// 			}
+	// 		}
+	// 		if (!chan_exist)
+	// 		{
+	// 			std::cerr << "Error : no such channel as " << chans[j] << "." << std::endl;
+	// 			users[j] = "";
+	// 			chans[j] = "";
+	// 		}
+	// 	}
+	// }
+
+	// ERR_BADCHANMASK //
+
+	// ERR_CHANOPRIVSNEEDED //
+	
+	// ERR_NOTONCHANNEL //
+
+	// Lancer la commande KICK //
 }
 
 void pars_mode(std::string str, Server &serv)
@@ -304,7 +488,7 @@ void pars_mode(std::string str, Server &serv)
 	std::string param;
 	size_t i = 0;
 
-	while (i < str.find(" "))
+	while (i < str.find(' ') && i < str.size())
 	{
 		channel += str[i];
 		i++;
@@ -322,7 +506,7 @@ void pars_mode(std::string str, Server &serv)
 		i++;
 	}
 
-	if (mod.empty() || mod.size() != 2 || mod[0] != '+' || mod[0] != '-')
+	if (mod.empty() || mod.size() != 2 || (mod[0] != '+' && mod[0] != '-'))
 	{
 		std::cerr << "Error with mod." << std::endl;
 		return ;
@@ -353,12 +537,77 @@ void pars_mode(std::string str, Server &serv)
 	// 	return ;
 	// }
 
+	// ERR_CHANOPRIVSNEEDED //
+	
+	// ERR_NOCHANMODES //
+
+	// ERR_USERNOTINCHANNEL //
+
 	// Lancer la commande MODE //
 }
 
-void pars_privmsg(std::string str)
+void pars_privmsg(std::string str, Server &serv)
 {
-	 
+	std::string target;
+	std::string message;
+	size_t i = 0;
+
+	if (str.empty())
+	{
+		std::cerr << "Error : no target given." << std::endl;
+		return ;
+	}
+
+	while (i < str.find(' ') && i < str.size())
+	{
+		target += str[i];
+		i++;
+	}
+
+	i++;
+	while (i < str.size())
+	{
+		message += str[i];
+		i++;
+	}
+
+	if (message.empty())
+	{
+		std::cerr << "Error : no message given." << std::endl;
+		return ;
+	}
+
+	std::string nick;
+	i = 0;
+	while (i < target.find('!') && i < target.find('@') && i < target.find('%') && i < target.size())
+	{
+		nick += target[i];
+		i++;
+	}
+	
+	std::vector<Client> clients = serv.getClients();
+	bool nick_exist = 0;
+	
+	for (size_t i = 0; i < clients.size(); i++)
+	{
+		if (nick == clients[i].getNickname())
+			nick_exist = 1;
+	}
+	if (!nick_exist)
+	{
+		std::cerr << "Error : no such nickname on the server." << std::endl;
+		return ;
+	}
+
+	// ERR_CANNOTSENDTOCHAN //
+
+	// ERR_NOTOPLEVEL //
+
+	// ERR_WILDTOPLEVEL //
+
+	// ERR_TOOMANYTARGETS //
+
+	// Lancer la commande PRIVMSG //
 }
 
 
@@ -370,12 +619,12 @@ void parsing(std::string str, Server &serv)
 	if (str[0] == ':')
 	{
 		i++;
-		while (str[i] && str[i] != ' ')
+		while (str[i] && i < str.find(' '))
 		{
 			prefix += str[i];
 			i++;
 		}
-		if (str[i] == ' ')
+		if (i == str.find(' '))
 			i++;
 	}
 
@@ -424,31 +673,33 @@ void parsing(std::string str, Server &serv)
 			break;
 		case 2:
 			if (args == serv.getpasswd())
-				// Lancer la commande Password //
+				;// Lancer la commande Password //
+			else
+				std::cerr << "Error : wrong password." << std::endl;
 			break;
 		case 3:
 			pars_join(args, serv);
 			break;
 		case 4:
-			
+			pars_part(args, serv);
 			break;
 		case 5:
-			
+			pars_topic(args, serv);
 			break;
 		case 6:
-			
+			pars_invite(args, serv);
 			break;
 		case 7:
-			
+			pars_kick(args, serv);
 			break;
 		case 8:
-			
+			// Lancer la commande QUIT //
 			break;
 		case 9:
 			pars_mode(args, serv);
 			break;
 		case 10:
-			
+			pars_privmsg(args, serv);
 			break;
 		case 11:
 			std::cerr << "Error, please provide one of these commands : NICK, USER, PASS, JOIN, PART, TOPIC, INVITE, KICK, QUIT, MODE or PRIVMSG" << std::endl;
